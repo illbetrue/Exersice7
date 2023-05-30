baaefac022a72df48aaf054c0a9c211e64e964b9
Dzmitry Shaplyka <dzmitry_shaplyka@epam.com>
2023-05-30 11:40:30 +0000

    Update pageObject.js

--- src/pageObject.js
+++ src/pageObject.js
@@ -4,20 +4,22 @@
  * - https://martinfowler.com/bliki/PageObject.html
  * - https://webdriver.io/docs/pageobjects/
  * - https://javascript.info/class-inheritance
- * Inctruction:
- * 1. LoginPage should be inherited from the BasePage
- * 2. BasePage should accept one argument 'url'
- * 3. LoginPage should use the parent constructor and extend it with one argument 'name'
- * 4. BasePage should have open() method that will get 'pageName' as a parameter
+ * Instruction:
+ * 1. Create class BasePage
+ * 2. BasePage should accept only one argument 'url'
+ * 3. BasePage should have method 'open()' that will get 'pageName' as a parameter 
  * and return the string: "Open this ${this.url}/${pageName}"
- * 5. Login page should have open() method that will call the parent's open() method passing
+ * 4. Create class LoginPage
+ * 5. LoginPage should be inherited from the BasePage
+ * 6. LoginPage should use the parent constructor and extend it with one argument 'name'
+ * 7. LoginPage should have 'open()' method that will call the parent's 'open()' method passing
  * 'this.name' as a parameter
- * 6. Component class should accept one argument 'type'
- * (will receive values such as 'footer' and 'header')
- * 7. BasePage should have getters 'footer()' and 'header()' that will return new Component
+ * 8. Create class Component that should accept one argument 'type' (will receive values such as 'footer' and 'header')
+ * 9. Component should have 'review()' method and return the string "I can review ${this.type}"
+ * 10. BasePage should have getters 'footer()' and 'header()' that will return new Component
  * passing 'footer' and 'header' respectively ( e.g return new Component('footer') )
- * 8. Component should have review() method that will return the string "I can review ${this.type}"
  */
+
 
 class BasePage {}
 
